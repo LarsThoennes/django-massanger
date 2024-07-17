99aa1c94f85f05a6bc6beef0a442db7390a59329
LarsThoennes <larsthoennes@gmail.com>
2024-07-17 12:01:04 +0200

    show message

--- django_chat_app/chat/views.py
+++ django_chat_app/chat/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from .models import Message, Chat
 
 def index(request):
     if request.method == 'POST':
         print("Received Data " + request.POST['textmessage'])
-    return render(request, 'chat/index.html', {'username' : 'Lars'})
-
+        myChat = Chat.objects.get(id=1)
+        Message.objects.create(text = request.POST['textmessage'], chat=myChat, author=request.user, receiver=request.user)
+    chatMessages = Message.objects.filter(chat__id = 1)
+    return render(request, 'chat/index.html', {'messages' : chatMessages})
